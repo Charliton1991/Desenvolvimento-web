a0e3f286c6f77c4ba7d4b73c858bd4152d9896a2
Charliton <charliton1991gpi@gmail.com>
2026-06-13 13:45:33 -0300

    Receita-waffles

---
+++ Ex007/script.js
@@ -0,0 +1,7 @@
+function cadastro() {
+    let pnome = document.getElementById('pnome').value;}
+    window.alert(`${pnome} seu cadasto foi realizado com sucesso`);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<p>Sr. ${pnome}, seu cadastro foi realizado com sucesso!</p>`
+    resultado.style.backgroundColor = 'red'
+    window.alert('cadastro realizado com sucesso!!!');
